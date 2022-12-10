81bd86ffe90ed82ef075e9ba60612d19dd3285b3
Aayush <58940228+aaks-ctrl@users.noreply.github.com>
2022-12-10 16:57:54 +0530

    repo cleanup

--- src/components/index.js
+++
@@ -1,4 +0,0 @@
-export {default as NavBar} from './NavBar';
-export {default as MovieCard} from './MovieCard';
-export {default as MovieList} from './MovieList';
-export {default as BlankPage} from './BlankPage';
